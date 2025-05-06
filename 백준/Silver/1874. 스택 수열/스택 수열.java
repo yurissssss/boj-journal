558889269998c926f3ab4eaf61be564adf2d0784
YURI <yur1n9@kakao.com>
2025-05-06 23:37:57 +0900

    [Silver II] Title: 스택 수열, Time: 848 ms, Memory: 106620 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -13,47 +13,34 @@ public class Main {
         }
 
         Stack<Integer> stack = new Stack<>();
-        //연산 기록 - 빠른 문자열 처리
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();  //연산 기록 - 빠른 문자열 처리
 
-        //오름차순으로 push할 숫자 - 1부터 시작
-        int num = 1;
-        //수열을 만들 수 있는지 판단
-        boolean result = true;
+        int num = 1;  //오름차순으로 push할 숫자 - 1부터 시작
+        boolean result = true;  //수열을 만들 수 있는지 판단
 
         for (int i=0; i<A.length; i++) {
-            //현재 수열의 수 - 이번에 만들어야 할 수 (목표 숫자)
-            int element = A[i];
-            //1️⃣ 현재 수열 값 >= 오름차순 자연수 (현재 목표 숫자가 아직 스택에 안 들어간 경우)
-            if (element >= num) {
-                //값이 같아질 때까지 push() 수행
-                while (element >= num) {
+            int element = A[i];  //현재 수열의 수 - 이번에 만들어야 할 수 (목표 숫자)
+            if (element >= num) {  //1️⃣ 현재 수열 값 >= 오름차순 자연수 (현재 목표 숫자가 아직 스택에 안 들어간 경우)
+                while (element >= num) {  //값이 같아질 때까지 push() 수행
                     stack.push(num++);
                     sb.append("+\n");  //push 연산 기록
                 }
-                //목표 숫자에 도달 -> pop
-                stack.pop();
+                stack.pop();  //목표 숫자에 도달 -> pop
                 sb.append("-\n");  //pop 연산 기록
             }
-            //2️⃣ 현재 수열 값 < 오름차순 자연수 (현재 목표 숫자가 이미 스택 안에 있는 경우)
-            else {
+            else {  //2️⃣ 현재 수열 값 < 오름차순 자연수 (현재 목표 숫자가 이미 스택 안에 있는 경우)
                 int n = stack.pop();  //스택에서 숫자 꺼내기
 
-                //만약 꺼낸 숫자(top)가 필요한 숫자보다 크면 수열을 출력할 수 X
-                // (LIFO구조 -> pop 순서 바꿀 수 X -> 원하는 순서대로 꺼낼 수 X)
-                // -> NO
-                if (n > element) {
+                if (n > element) {  //만약 꺼낸 숫자(top)가 필요한 숫자보다 크면 수열을 출력할 수 X -> NO
                     System.out.println("NO");
                     result = false;
                     break;
                 }
                 else {
-                    //원하는 숫자와 일치 -> 정상적인 pop
-                    sb.append("-\n");
+                    sb.append("-\n");  //원하는 숫자와 일치 -> 정상적인 pop
                 }
             }
         }
-        //수열을 성공적으로 만들었다면 결과 출력
-        if (result) System.out.println(sb.toString());
+        if (result) System.out.println(sb.toString());  //수열을 성공적으로 만들었다면 결과 출력
     }
 }
