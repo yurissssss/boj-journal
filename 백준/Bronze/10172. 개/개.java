842c7db19682a16a3d7d8e3eadc897e6a9ab0e86
YURI <yur1n9@kakao.com>
2025-11-17 21:06:51 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        // ''', '"', '\' 를 출력하고 싶을 때는 앞에 '\' 붙여주기!
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
