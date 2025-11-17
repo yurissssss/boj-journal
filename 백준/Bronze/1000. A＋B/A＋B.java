dcdc9eabdc85c42cddb066a5412941b7b6d00782
YURI <yur1n9@kakao.com>
2025-11-17 19:30:37 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] str) {
         Scanner sc = new Scanner(System.in);
         
         int A = sc.nextInt();
         int B = sc.nextInt();
         
-        int answer = A + B;
-        
         System.out.println(A+B);
-        
         sc.close();
     }
 }
