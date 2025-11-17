73df2a75535d4967816a5758c0a177e1a722ba21
YURI <yur1n9@kakao.com>
2025-11-17 19:41:17 +0900

    [Bronze V] Title: A-B, Time: 168 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] str) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println(sc.nextInt() - sc.nextInt());
+        sc.close();
+    }
+}
