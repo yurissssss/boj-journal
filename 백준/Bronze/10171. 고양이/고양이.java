ccef6e4551db02734d3bde25a48f897ed91588ba
YURI <yur1n9@kakao.com>
2025-11-17 21:00:55 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13888 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) {
+        // '역슬래시(\)' 사용하려면 앞에 '\' 한 번 더 붙여주기! 
         System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
