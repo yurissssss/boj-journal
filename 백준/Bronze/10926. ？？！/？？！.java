6d70bd1e4520e1f1f04ef2cc2b0f20f4d63b3bc7
YURI <yur1n9@kakao.com>
2026-02-25 13:50:36 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] str) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        
+        String st = "??!";
+        
+        System.out.println(sc.next() + st);
+        
+        sc.close();
+    }
+}
