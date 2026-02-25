e39ed41d1954ebb5983fbbef175ba1df4bbdf8be
YURI <yur1n9@kakao.com>
2026-02-25 13:37:21 +0900

    [Bronze V] Title: 사칙연산, Time: 180 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] str){
         Scanner sc = new Scanner(System.in);
         
         int A = sc.nextInt();
         int B = sc.nextInt();
         
-        System.out.println(A + B);
-        System.out.println(A - B);
-        System.out.println(A * B);
-        System.out.println(A / B);
-        System.out.println(A % B);
+        sc.close();
+        
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
     }
 }
