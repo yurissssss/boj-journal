b0ea6d6fe2f4538d289e4a7fb1725ab406d5bf6d
YURI <yur1n9@kakao.com>
2026-02-26 10:48:43 +0900

    [Bronze V] Title: 나머지, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,3 +1,5 @@
+/* 📌 Scanner 사용
+
 import java.util.Scanner;
 import java.io.IOException;
 
@@ -10,6 +12,30 @@ public class Main{
         int C = sc.nextInt();
         
         sc.close();
+        
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
+*/
+    
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] str) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] input = br.readLine().split(" ");
+        
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+        
+        br.close();
         
         System.out.println((A+B)%C);
         System.out.println(((A%C) + (B%C))%C);
