63d2260fb69573a93ee468154f3064694123b935
YURI <yur1n9@kakao.com>
2026-02-28 17:53:25 +0900

    [Bronze V] Title: A+B - 4, Time: 112 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,18 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        while(true) {
-            try {
-                int a = sc.nextInt();
-                int b = sc.nextInt();
-                
-                System.out.println(a + b);
-            } catch(Exception e) {
-                break;
-            }
+        StringTokenizer st;
+        
+        int a = 1;
+        int b = 1;
+        
+        String nums;
+        
+        while((nums = br.readLine()) != null) {
+            st = new StringTokenizer(nums);
+            
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            
+            bw.write((a+b) + "\n");
         }
+        
+        br.close();
+        bw.close();
     }
 }
