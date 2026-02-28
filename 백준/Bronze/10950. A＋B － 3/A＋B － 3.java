0fec56e5bdd7e7f32966d606a35e10a80fbe4f7e
YURI <yur1n9@kakao.com>
2026-02-28 16:07:46 +0900

    [Bronze V] Title: A+B - 3, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,15 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int T = sc.nextInt();
+        int num = Integer.parseInt(br.readLine());
         
-        for (int i = 0; i < T; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
             System.out.println(a+b);
         }
+        
+        br.close();
     }
 }
