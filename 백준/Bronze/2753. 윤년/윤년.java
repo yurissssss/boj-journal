b6d806f033f36279eb35b4cb8564c795b55a0fbd
YURI <yur1n9@kakao.com>
2026-02-28 14:46:42 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int n = sc.nextInt();
+        int year = Integer.parseInt(br.readLine());
         
-        System.out.println(((n%4==0) && (n%100!=0)) ? 1 : ((n%400==0) ? 1 : 0));
+        br.close();
+        
+       if (((year%4 == 0) && (year%100 != 0)) || year%400 == 0) System.out.println("1");
+        else System.out.println("0");
     }
 }
