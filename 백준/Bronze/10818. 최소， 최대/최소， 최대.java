9701c1e4eb996a613326f64bad29af31fb823713
YURI <yur1n9@kakao.com>
2026-02-28 18:24:34 +0900

    [Bronze III] Title: 최소, 최대, Time: 1260 ms, Memory: 126840 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] str) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        String[] st = br.readLine().split(" ");
+        
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st[i]);
+        }
+        
+        Arrays.sort(arr);
+        
+        bw.write(arr[0] + " " + arr[n-1]);
+        
+        br.close();
+        bw.close();
+    }
+}
