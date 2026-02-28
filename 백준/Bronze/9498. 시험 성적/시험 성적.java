09f9c653faefdb5492cfe49b694fab56c4b79503
YURI <yur1n9@kakao.com>
2026-02-28 14:41:27 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int score = sc.nextInt();
+        int score = Integer.parseInt(br.readLine());
         
-        if (90 <= score && score <= 100) System.out.println("A");
-        else if (80 <= score && score <= 89) System.out.println("B");
-        else if (70 <= score && score <= 79) System.out.println("C");
-        else if (60 <= score && score <= 69) System.out.println("D");
+        br.close();
+        
+        if (score <= 100 && score >= 90) System.out.println("A");
+        else if (score <= 89 && score >= 80) System.out.println("B");
+        else if (score <= 79 && score >= 70) System.out.println("C");
+        else if (score <= 69 && score >= 60) System.out.println("D");
         else System.out.println("F");
     }
 }
