64de3a84f0575dee0b1e64957fef1e84bbe991d7
YURI <yur1n9@kakao.com>
2026-02-28 18:14:14 +0900

    [Bronze V] Title: X보다 작은 수, Time: 148 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,15 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int N = sc.nextInt();
-        int X = sc.nextInt();
+        String[] arr = br.readLine().split(" ");
+        int n = Integer.parseInt(arr[0]);
+        int x = Integer.parseInt(arr[1]);
         
-        for (int i = 0; i < N; i++) {
-            int num = sc.nextInt();
-            if (num < X) System.out.print(num + " ");
+        String[] st = br.readLine().split(" ");
+        
+        for (int i = 0; i < n; i++) {
+            if (Integer.parseInt(st[i]) < x) bw.write(st[i] + " ");
         }
+        
+        br.close();
+        bw.close();
     }
 }
