98ed42378108824da01aedd0746b2ccd1e98da5d
YURI <yur1n9@kakao.com>
2026-02-28 17:36:36 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,21 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int n = sc.nextInt();
+        int num = Integer.parseInt(br.readLine());
         
-        int i = 0;
-        
-        while (i < n) {
-            i++;
-            
+        for (int i = 1; i <= num; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                bw.write("*");
             }
-            
-            System.out.println();
+            bw.write("\n");
         }
+        
+        br.close();
+        bw.close();
     }
 }
