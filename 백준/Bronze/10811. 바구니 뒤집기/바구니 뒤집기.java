d5a0f1fdcd5f02ba9799119952778f855b1705b3
YURI <yur1n9@kakao.com>
2026-03-04 10:22:45 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 15940 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            StringTokenizer nums = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(nums.nextToken());
+            int b = Integer.parseInt(nums.nextToken());
+            
+            for (int j = a-1; j < (a+b-1)/2; j++) {
+                int temp = arr[a+b-j-2];
+                arr[a+b-j-2] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i] + " ");
+        }
+        
+        br.close();
+        bw.close();
+        
+    }
+}
