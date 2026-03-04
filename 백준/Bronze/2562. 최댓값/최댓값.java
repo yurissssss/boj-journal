a7e612bd07154e64bf05a2dd58c8b870d74cc18d
YURI <yur1n9@kakao.com>
2026-03-04 09:17:23 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,33 +1,26 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.IOException;
-import java.util.Arrays;
-
 public class Main{
     public static void main(String[] str) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int[] origin = new int[9];
-        int[] arr = new int[9];
-        
-        for (int i = 0; i < 9; i++) {
-            origin[i] = Integer.parseInt(br.readLine());
-            arr[i] = origin[i];
-        }
-        
-        Arrays.sort(arr);
+        int max = Integer.MIN_VALUE;
+        int num = 0;
+        int index = 0;
         
         for (int i = 0; i < 9; i++) {
-            if (origin[i] == arr[8]) {
-                bw.write(origin[i] + "\n" + (i+1) + "\n");
-                break;
+            num = Integer.parseInt(br.readLine());
+            
+            if (num > max) {
+                max = num;
+                index = i + 1;
             }
         }
         
         br.close();
-        bw.close();
+        
+        System.out.println(max);
+        System.out.println(index);
     }
 }
