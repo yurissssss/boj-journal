f9973aa050fcf806178909e21cc2f4fe6d3c2cdc
YURI <yur1n9@kakao.com>
2026-03-05 21:53:44 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -8,35 +8,13 @@ public class Main{
         
         String st = br.readLine();
         
-        int answer = 0;
-        int num = 0;
+        String[] cro = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
         
-        while(num < st.length()) {
-            char alp = st.charAt(num);
-            
-            if (alp == 'c') {
-                if ((num + 1 < st.length()) && ((st.charAt(num + 1) == '=') || (st.charAt(num + 1) == '-'))) {
-                    num += 2;
-                } else num++;
-            } else if (alp == 'd') {
-                if ((num + 2 < st.length()) && (st.charAt(num + 1) == 'z') && (st.charAt(num + 2)) == '=') {
-                    num += 3;
-                } else if ((num + 1 < st.length()) && st.charAt(num + 1) == '-') num += 2;
-                else num++;
-            } else if (alp == 'l' || alp == 'n') {
-                if ((num + 1 < st.length()) && (st.charAt(num + 1) == 'j')) {
-                    num += 2;
-                } else num++;
-            } else if (alp == 's' || alp == 'z') {
-                if ((num + 1 < st.length()) && (st.charAt(num + 1) == '=')) {
-                    num += 2;
-                } else num++;
-            } else num++;
-            
-            answer++;
+        for (String s : cro) {
+            st = st.replace(s, "*");
         }
         
-        System.out.println(answer);
+        System.out.println(st.length());
         
         br.close();
     }
