4330cb8eaa735e7782cebc9c6ee738d4343d210f
YURI <yur1n9@kakao.com>
2026-03-06 16:32:29 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int num = Integer.parseInt(br.readLine());
+        
+        int[][] block = new int[100][100];
+        
+        for (int i=0; i<num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            for (int j=x; j<x+10; j++) {
+                for (int k=y; k<y+10; k++) block[j][k] = 1;
+            }
+        }
+        
+        int count = 0;
+        for (int i=0; i<100; i++) {
+            for (int j=0; j<100; j++) {
+                if (block[i][j] == 1) count++;
+            }
+        }
+        
+        System.out.println(count);
+        
+        br.close();
+    }
+}
