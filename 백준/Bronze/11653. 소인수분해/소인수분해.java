bc67bafb4bf2f7689811dc1c7cee9177e975d13e
YURI <yur1n9@kakao.com>
2026-03-08 22:32:43 +0900

    [Bronze I] Title: 소인수분해, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args) throws IOException {    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 2; i*i <= n; i++) {
+            while (n%i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+        
+        if (n > 1) System.out.println(n);
+        
+        br.close();
+    }
+}
