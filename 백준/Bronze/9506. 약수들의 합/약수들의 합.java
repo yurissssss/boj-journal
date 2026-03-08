d2c7db6b69538db58ddb60f2624dbd52133af0e9
YURI <yur1n9@kakao.com>
2026-03-08 18:53:46 +0900

    [Bronze I] Title: 약수들의 합, Time: 124 ms, Memory: 15764 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] str) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = 0;
+        
+        while((n = Integer.parseInt(br.readLine())) != -1) {
+            findSum(n);
+        }
+        
+        br.close();
+    }
+    
+    public static void findSum(int n) {
+        int sum = 1;
+        
+        StringBuilder sb = new StringBuilder(n + " = 1");
+        
+        for (int i=2; i<n; i++) {
+            if ((n%i == 0)) {
+                sum += i;
+                sb.append(" + " + i);
+            }
+        }
+        
+        String answer;
+        
+        if (sum == n) {
+            answer = sb.toString();
+        } else answer = (n + " is NOT perfect.");
+            
+        System.out.println(answer);
+    }
+}
