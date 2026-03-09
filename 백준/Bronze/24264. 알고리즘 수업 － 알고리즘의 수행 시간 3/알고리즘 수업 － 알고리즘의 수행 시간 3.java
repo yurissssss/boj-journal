5ad15d6e9f23d5a96bdaf1ad6c91edf3a5a9e704
YURI <yur1n9@kakao.com>
2026-03-09 20:23:43 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
