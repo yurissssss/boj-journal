d7092493b940d634e9316663ac2e6e1419c1b6e2
YURI <yur1n9@kakao.com>
2026-03-09 20:10:11 +0900

    [Bronze III] Title: 세 막대, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.java
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -2,16 +2,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] st = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] side = new int[3];
         
         for (int i = 0; i < 3; i++) {
-            side[i] = Integer.parseInt(st[i]);
+            side[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(side);
         
@@ -22,7 +23,7 @@ public class Main {
     }
     
     private static int makeTri(int a, int b, int c) {
-        if ((a + b) <= c) c = a + b - 1;
-        return (a + b + c);
+        if ((a + b) <= c) return (a + b) * 2 - 1;
+        return a + b + c;
     }
 }
