6c5f0b806a1fdebe1ba2bef9298403f8809e2bbc
YURI <yur1n9@kakao.com>
2026-03-10 21:06:45 +0900

    [Bronze II] Title: 분해합, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -7,14 +7,15 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
-        int start = n - ((int)Math.log10(n) + 1) * 9;
+        // n = m + (자릿수 합)
+        int start = n - String.valueOf(n).length() * 9;
         if (start < 1) start = 1;
-
+        
         for (int i = start; i < n; i++) {
             int sum = i;
             
-            for (int x = i; x > 0; x /= 10) {
-                sum += x % 10;
+            for (int j = i; j > 0; j /= 10) {
+                sum += j % 10;
             }
             
             if (sum == n) {
