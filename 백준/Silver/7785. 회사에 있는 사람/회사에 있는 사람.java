096229ff94fe0fec124340b2dba5c0373111acc6
YURI <yur1n9@kakao.com>
2026-03-13 20:48:29 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 776 ms, Memory: 60976 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -4,32 +4,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int n = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
         
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            String[] st = br.readLine().split(" ");
             
-            String name = st.nextToken();
-            String state = st.nextToken();
-            
-            if (state.equals("enter")) set.add(name);
-            else set.remove(name);
+            if (st[1].equals("enter")) set.add(st[0]);
+            else set.remove(st[0]);
         }
         
         List<String> list = new ArrayList<>(set);
         
         Collections.sort(list, Collections.reverseOrder());
         
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < list.size(); i++) {
-            sb.append(list.get(i)).append('\n');
+        for (String s : list) {
+            bw.write(s + '\n');
         }
         
-        System.out.print(sb);
-        
+        bw.flush();
         br.close();
+        bw.close();
     }
 }
