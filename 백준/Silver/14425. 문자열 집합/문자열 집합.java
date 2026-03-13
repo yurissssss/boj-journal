93bdbf716ca112663718effe49f515b306b9b604
YURI <yur1n9@kakao.com>
2026-03-13 20:35:59 +0900

    [Silver IV] Title: 문자열 집합, Time: 388 ms, Memory: 31332 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Set<String> set = new HashSet<>();
+        
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+        
+        int answer = 0;
+        
+        for (int i = 0; i < m; i++) {
+            String target = br.readLine();
+            
+            if (set.contains(target)) answer++;
+        }
+        
+        System.out.println(answer);
+        
+        br.close();
+    }
+}
