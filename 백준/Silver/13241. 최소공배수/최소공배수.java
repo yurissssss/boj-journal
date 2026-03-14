06eaca366ea0766d0e5fba82ecc13d1dfd3f398c
YURI <yur1n9@kakao.com>
2026-03-14 21:08:12 +0900

    [Silver V] Title: 최소공배수, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        
+        long lcm = (a * b) / gcd(a, b);
+        
+        System.out.println(lcm);
+        
+        br.close();
+    }
+    
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        
+        return a;
+    }
+}
