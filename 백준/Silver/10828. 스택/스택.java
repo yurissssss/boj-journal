3ee09f4c6704312439dfc6d88b79d1cb2d348435
YURI <yur1n9@kakao.com>
2026-03-18 22:47:46 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 18896 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+            
+            switch(order) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    stack.push(x);
+                    break;
+                    
+                case "pop":
+                    sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
+                    break;
+                    
+                case "size":
+                    sb.append(stack.size()).append('\n');
+                    break;
+                    
+                case "empty":
+                    sb.append(stack.isEmpty() ? 1 : 0).append('\n');
+                    break;
+                    
+                case "top":
+                    sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+            }
+        }
+        
+        System.out.print(sb);
+        
+        br.close();
+    }
+}
