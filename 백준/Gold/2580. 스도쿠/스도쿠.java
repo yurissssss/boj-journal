178c2e1373c7d9b08fa6a09d004a2a7d0aa90bd2
YURI <yur1n9@kakao.com>
2026-03-19 18:07:09 +0900

    [Gold IV] Title: 스도쿠, Time: 500 ms, Memory: 22236 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -2,13 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int[][] arr = new int[9][9];
+    static int[][] arr;
     static List<int[]> empty = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        
+        arr = new int[9][9];
         
         for (int i = 0; i < 9; i++) {
             st = new StringTokenizer(br.readLine());
@@ -26,34 +28,44 @@ public class Main {
     private static void dfs(int depth) {
         if (depth == empty.size()) {
             for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 9; j++) sb.append(arr[i][j]).append(" ");
+                for (int j = 0; j < 9; j++) {
+                    sb.append(arr[i][j]).append(" ");
+                }
                 sb.append("\n");
             }
             System.out.print(sb);
+            
+            // 'return;' 하면 상위 depth의 dfs로 돌아감
+            // 다른 문제들은 모든 경우의 수를 탐색해서 'return;'이 가능했지만
+            // 여기서는 빈칸만 탐색하므로 불가능!
             System.exit(0);
         }
         
-        int row = empty.get(depth)[0];
-        int col = empty.get(depth)[1];
+        int[] now = empty.get(depth);
+        int row = now[0];
+        int col = now[1];
         
         for (int i = 1; i <= 9; i++) {
             arr[row][col] = i;
             
-            if (isPossible(row, col)) dfs(depth + 1);
+            if (isPossible(row, col)) {
+                dfs(depth + 1);
+            }
             
+            // 만약 실패하고 돌아올 경우 -> 초기화
             arr[row][col] = 0;
         }
     }
     
-    private static boolean isPossible(int r, int c) {
+    private static boolean isPossible(int row, int col) {
         for (int i = 0; i < 9; i++) {
-            if ((i != r) && (arr[i][c] == arr[r][c])) return false;
-            if ((i != c) && (arr[r][i] == arr[r][c])) return false;
+            if (i != col && arr[row][i] == arr[row][col]) return false;
+            if (i != row && arr[i][col] == arr[row][col]) return false;
         }
         
-        for (int i = r / 3 * 3; i < r / 3 * 3 + 3; i++) {
-            for (int j = c / 3 * 3; j < c / 3 * 3 + 3; j++) {
-                if (!(i == r && j == c) && (arr[i][j] == arr[r][c])) return false;
+        for (int i = row / 3 * 3; i < row / 3 * 3 + 3; i++) {
+            for (int j = col / 3 * 3; j < col / 3 * 3 + 3; j++) {
+                if (!(i == row && j == col) && arr[row][col] == arr[i][j]) return false;
             }
         }
         
