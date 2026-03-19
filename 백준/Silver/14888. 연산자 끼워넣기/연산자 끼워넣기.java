34a5950041fcf61d49ff0131409a5dd08ba9cf7a
YURI <yur1n9@kakao.com>
2026-03-19 18:55:25 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 112 ms, Memory: 16416 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -6,7 +6,7 @@ public class Main {
     static int[] nums;
     static int[] codes = new int[4];
     static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
+    static int min = Integer.MAX_VALUE; 
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,13 +16,11 @@ public class Main {
         nums = new int[n];
         
         st = new StringTokenizer(br.readLine());
-        
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
         
         st = new StringTokenizer(br.readLine());
-        
         for (int i = 0; i < 4; i++) {
             codes[i] = Integer.parseInt(st.nextToken());
         }
@@ -42,28 +40,28 @@ public class Main {
             return;
         }
         
-            if (codes[0] > 0) {
-                codes[0]--;
-                dfs(depth + 1, value + nums[depth]);
-                codes[0]++;
-            }
-            
-            if (codes[1] > 0) {
-                codes[1]--;
-                dfs(depth + 1, value - nums[depth]);
-                codes[1]++;
-            }
-            
-            if (codes[2] > 0) {
-                codes[2]--;
-                dfs(depth + 1, value * nums[depth]);
-                codes[2]++;
-            }
-            
-            if (codes[3] > 0) {
-                codes[3]--;
-                dfs(depth + 1, value / nums[depth]);
-                codes[3]++;
-            }
+        if (codes[0] > 0) {
+            codes[0]--;
+            dfs(depth + 1, value + nums[depth]);
+            codes[0]++;
+        }
+        
+        if (codes[1] > 0) {
+            codes[1]--;
+            dfs(depth + 1, value - nums[depth]);
+            codes[1]++;
+        }
+        
+        if (codes[2] > 0) {
+            codes[2]--;
+            dfs(depth + 1, value * nums[depth]);
+            codes[2]++;
+        }
+        
+        if (codes[3] > 0) {
+            codes[3]--;
+            dfs(depth + 1, value / nums[depth]);
+            codes[3]++;
+        }
     }
 }
