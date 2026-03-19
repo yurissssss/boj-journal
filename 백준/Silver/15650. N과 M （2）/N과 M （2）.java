fca0cea32e35d2f9af93ba13243728f461ee3ec7
YURI <yur1n9@kakao.com>
2026-03-19 14:44:47 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        visited = new boolean[n + 1];
+        
+        dfs(n, m, 0, 1);
+        
+        System.out.print(sb);
+        
+        br.close();
+    }
+    
+    private static void dfs(int n, int m, int depth, int start) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) sb.append(arr[i]).append(' ');
+            sb.append('\n');
+            return;
+        }
+        
+        for (int i = start; i <= n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i;
+                
+                dfs(n, m, depth + 1, i + 1);
+                
+                visited[i] = false;
+            }
+        }
+    }
+}
