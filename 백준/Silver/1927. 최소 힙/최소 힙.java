44e5e466c93b9054cb13b1133b1b13fec4016d55
YURI <yur1n9@kakao.com>
2026-03-24 21:41:25 +0900

    [Silver II] Title: 최소 힙, Time: 292 ms, Memory: 26688 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            
+            if (x == 0) {
+                if (pq.isEmpty()) sb.append(0).append('\n');
+                else sb.append(pq.poll()).append('\n');
+            } else pq.offer(x);
+        }
+        
+        System.out.print(sb);
+        br.close();
+    }
+}
