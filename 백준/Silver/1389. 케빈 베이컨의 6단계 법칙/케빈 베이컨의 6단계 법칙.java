e9f6cdbe06fe3c34cd47c54805ed699e18cc1018
YURI <yur1n9@kakao.com>
2026-03-25 19:02:18 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 128 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static List<Integer>[] graph;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());  // 친구 관계
+        
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        
+        int min = Integer.MAX_VALUE;
+        int answer = 1;
+        for (int i = 1; i <= n; i++) {
+            int sum = bfs(i, n);
+            
+            if (min > sum) {
+                min = sum;
+                answer = i;
+            }
+        }
+        
+        System.out.print(answer);
+        br.close();
+    }
+    
+    private static int bfs(int start, int n) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1];
+        int[] dist = new int[n + 1];
+        
+        dq.offer(start);
+        visited[start] = true;
+        
+        while (!dq.isEmpty()) {
+            int cur = dq.poll();
+            
+            for (int next : graph[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    dist[next] = dist[cur] + 1;
+                    dq.offer(next);
+                }
+            }
+        }
+        
+        int sum = 0; 
+        for (int i = 1; i <= n; i++) {
+            sum += dist[i];
+        }
+        
+        return sum;
+    }
+}
