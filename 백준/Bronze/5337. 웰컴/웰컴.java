41032c3ca91f967cd3eea40dd1f4010003bf2b1a
YURI <yur1n9@kakao.com>
2026-03-31 20:42:22 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {    
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
