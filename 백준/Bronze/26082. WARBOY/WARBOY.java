c250b8836ffce2da79caf54d5b8f7240503b0d09
YURI <yur1n9@kakao.com>
2026-03-31 20:56:16 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        System.out.print(B * C * 3 / A);
+        br.close();
+    }
+}
