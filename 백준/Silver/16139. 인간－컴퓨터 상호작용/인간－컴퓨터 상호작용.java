0a95bfcadea029bd325a0e968698e7e79a03aa29
YURI <yur1n9@kakao.com>
2026-04-06 20:12:04 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 664 ms, Memory: 97408 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -8,20 +8,27 @@ public class Main {
         
         String s = br.readLine();
         int q = Integer.parseInt(br.readLine());
+        int[][] dp = new int[s.length() + 1][26];
+        
+        for (int i = 1; i <= s.length(); i++) {
+            int alp = s.charAt(i - 1) - 'a';
+            
+            for (int j = 0; j < 26; j++) {
+                dp[i][j] = dp[i - 1][j];
+            }
+            
+            dp[i][alp]++;
+        }
         
         for (int i = 0; i < q; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             
-            char a = st.nextToken().charAt(0);
+            int a = st.nextToken().charAt(0) - 'a';
             int l = Integer.parseInt(st.nextToken());
             int r = Integer.parseInt(st.nextToken());
             
-            int count = 0;
-            for (int j = l; j <= r; j++) {
-                if (s.charAt(j) == a) count++;
-            }
-            
-            sb.append(count).append('\n');
+            // dp[i]는 #1부터 시작 -> #(r + 1) = r번째 문자
+            sb.append(dp[r + 1][a] - dp[l][a]).append('\n');
         }
         
         System.out.print(sb);
