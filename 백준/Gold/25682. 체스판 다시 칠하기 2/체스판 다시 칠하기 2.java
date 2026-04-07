a8eec5338fbd1dcc3200e4556830c36aa1b7557c
YURI <yur1n9@kakao.com>
2026-04-07 17:43:14 +0900

    [Gold IV] Title: 체스판 다시 칠하기 2, Time: 368 ms, Memory: 39804 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.java
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.java
@@ -1,5 +1,7 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,27 +11,38 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        // dp: 현재 칸까지에서 새로 칠해야 하는 칸 수
+        // 현재 위치가 '0'인데 '-1'을 하면 #-1 -> #1부터 시작 
         int[][] dp = new int[N + 1][M + 1];
         
-        for (int i = 1; i < N + 1; i++) {
+        for (int i = 1; i <= N; i++) {
             String s = br.readLine();
             
             for (int j = 1; j <= M; j++) {
-                char exp = ((i + j) % 2 == 0) ? 'W' : 'B';
-                int wrong = s.charAt(j - 1) == exp ? 0 : 1;
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] 
-                    + wrong;
+                char c = s.charAt(j - 1);
+                // 'B'로 시작한다고 가정
+                // -> 'W'로 시작하는 경우 = 전체 체스판 크기 - 'B'로 시작하는 경우
+                // i + j: 윗칸도 고려
+                char exp = ((i + j) % 2 == 0) ? 'B' : 'W';
+                int wrong = c == exp ? 0 : 1;
+                
+                /*
+                oㅣ  x = - + ㅣ - o
+                -x   -> 앞 열 값 + 윗 행 값 - 공통된 부분
+                */
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + wrong;
             }
         }
         
-        int answer = N * M;
+        int answer = K * K;
         for (int i = K; i <= N; i++) {
             for (int j = K; j <= M; j++) {
+                // 전체 - 포함❎ 행 - 포함❎ 열 + 겹치는 부분
                 int count = dp[i][j] - dp[i - K][j] - dp[i][j - K] + dp[i - K][j - K];
+                
                 answer = Math.min(answer, Math.min(count, K * K - count));
             }
         }
-
         
         System.out.print(answer);
         br.close();
