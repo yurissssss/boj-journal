12a547542566022ae6ee4ceb162bb30d935f188d
YURI <yur1n9@kakao.com>
2026-05-10 14:55:13 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 8.00 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 86.8 MB, 시간: 30.77 ms
+메모리: 74.2 MB, 시간: 8.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 15일 18:49:38
+2026년 05월 10일 14:55:08
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,63 +1,32 @@
 import java.util.*;
 
-
-/**
-0s  [0 0] [7 4 5 6]
-1s  [0 7] [4 5 6]
-2s  [7 0] [4 5 6]
-3s  [0 4] [5 6]
-4s  [4 5] [6]
-5s  [5 0] [6]
-6s  [0 6] []
-7s  [6 0] []
-8s  [0 0] []
-
-선입선출 => 큐 사용
-*/
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        // 최종시간
-        int answer = 0;
+        Deque<Integer> bridge = new ArrayDeque<>();
         
-        // 다리를 칸 단위로 표현
-        Queue<Integer> bridge = new ArrayDeque<>();
-        for(int i = 0 ; i < bridge_length ; i++)
+        int idx = 0;
+        int time = 0;
+        int sum = 0;
+        
+        // 처음에 '0'으로 큐 채우기
+        // -> bridge_length초까지는 트럭이 아닌 '0'이 빠져나감
+        for (int i = 0; i < bridge_length; i++) {
             bridge.offer(0);
+        }
         
-        // 기존 트럭을 deque
-        Queue<Integer> waiting = new ArrayDeque<>();
-        for (int w : truck_weights)
-            waiting.offer(w);
-        
-        // 현 다리 위 총 무게
-        int currentWeight = 0;
-        
-        // 남은 트럭이 모두 소모
-        while (!waiting.isEmpty()){
-            // 1초씩 경과
-            answer++;
+        while (idx < truck_weights.length) {
+            time++;
             
-            // 다리 1칸 전진 -> 빠진 칸의 무게 제거
-            currentWeight -= bridge.poll();
+            sum -= bridge.poll();
             
-            // 다른 트럭을 올려도 무게 제한 넘지 않는다면
-            int next = waiting.peek();
-            if(currentWeight + next <= weight){
-                bridge.offer(waiting.poll());
-                // 총 무게 계산 (무게 갱신)
-                currentWeight += next;
-            }
-            
-            // 그렇지 않다면
-            else{
-                // 빈칸(0)만 추가로 넣어줌
+            if (sum + truck_weights[idx] <= weight) {
+                sum += truck_weights[idx];
+                bridge.offer(truck_weights[idx++]);
+            } else {
                 bridge.offer(0);
             }
         }
         
-        // 마지막 트럭이 빠져 나오는 데 필요한 시간 추가
-        answer += bridge_length;
-        
-        return answer;
+        return time + bridge_length;
     }
 }
