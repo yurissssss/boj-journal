fa0134bf394b450487f5b929d0b998cf7c20d275
YURI <yur1n9@kakao.com>
2026-07-29 18:14:27 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> clothMap = new HashMap<>();
-        
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
-            clothMap.put(clothes[i][1], clothMap.getOrDefault(clothes[i][1], 0) + 1);
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         
         int answer = 1;
-        
-        // map.values() : map의 value 모음
-        for (int i : clothMap.values()) {
+        for (int i : map.values()) {
             answer *= i + 1;
         }
         
