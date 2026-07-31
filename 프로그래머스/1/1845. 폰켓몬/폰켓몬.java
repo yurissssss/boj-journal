432e490c11ae6f92137deaecf12dc2bcb5a20f77
YURI <yur1n9@kakao.com>
2026-07-31 14:29:12 +0900

    [level 1] Title: 폰켓몬, Time: 3.18 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,8 +4,8 @@ class Solution {
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
         
-        for (int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+        for (int i : nums) {
+            set.add(i);
         }
         
         return Math.min(nums.length / 2, set.size());
