807a60827b1a458faa9b1e377435fe874796b5de
YURI <yur1n9@kakao.com>
2026-08-01 17:21:21 +0900

    [level 2] Title: 올바른 괄호, Time: 6.59 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,25 +2,18 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
         Deque<Character> dq = new ArrayDeque<>();
         
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                dq.offer(s.charAt(i));
-            } else {
-                if (dq.isEmpty()) {
-                    answer = false;
-                    break;
-                } else {
-                    dq.poll();
-                }
+            char cur = s.charAt(i);
+            if (cur == '(') dq.offer(cur);
+            else if (!dq.isEmpty()) dq.poll();
+            else {
+                return false;
             }
         }
         
-        if (!dq.isEmpty()) answer = false;
-
-        return answer;
+        if(dq.isEmpty()) return true;
+        else return false;
     }
 }
