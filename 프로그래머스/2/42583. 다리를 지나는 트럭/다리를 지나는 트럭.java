69c5fb67e442a5812ec041aff513983d96efc02a
YURI <yur1n9@kakao.com>
2026-08-08 19:06:59 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 6.86 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,28 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Deque<Integer> bridge = new ArrayDeque<>();
+        Deque<Integer> dq = new ArrayDeque<>();
         
-        int idx = 0;
-        int time = 0;
         int sum = 0;
+        int time = 0;
+        int idx = 0;
         
-        // 처음에 '0'으로 큐 채우기
-        // -> bridge_length초까지는 트럭이 아닌 '0'이 빠져나감
         for (int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);
+            dq.offer(0);
         }
         
         while (idx < truck_weights.length) {
             time++;
             
-            sum -= bridge.poll();
+            sum -= dq.poll();
             
             if (sum + truck_weights[idx] <= weight) {
                 sum += truck_weights[idx];
-                bridge.offer(truck_weights[idx++]);
+                dq.offer(truck_weights[idx++]);
             } else {
-                bridge.offer(0);
+                dq.offer(0);
             }
         }
         
